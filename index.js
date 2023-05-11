a345bed63b03814c05c0912884cb59b079f6367d
tw8130 <mwangitabitha2020@gmail.com>
2023-05-11 15:44:37 +0300

    updated the index.js by adding a while loop and alerting  errors for the random integer program

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // function to that prompts ser to enter random number that prog guesses if matched
-const compNumber = Math.floor(Math.random() * 11);
-const userNumber = prompt("Guess a number between 0 and 10");
+// const compNumber = Math.floor(Math.random() * 11);
+// const userNumber = prompt("Guess a number between 0 and 10");
+let numberTimes = 1;
 
-if (compNumber === userNumber) {
-    alert("Good work!");
-} else {
-    alert("Not matched " + "computer choose " + '' + '' +
-        compNumber);
+while (true) {
+    const compNumber = Math.floor(Math.random() * 11);
+    const userNumber = parseInt(prompt("Guess a number between 0 and 10"));
+    if (compNumber === userNumber) {
+        alert(`Good work! You were able to match with the computer that choose ${compNumber} after ${numberTimes} attempts.`);
+        break;
+    } else if (userNumber === '') {
+        alert("You did not put a number ! Try Again!")
+        continue;
+    } else if (isNaN(userNumber)) {
+        alert("That is not a number you entered");
+    } else {
+        alert("Not matched " + "computer choose " + '' + '' +
+            compNumber);
+        numberTimes++;
+    }
 }
